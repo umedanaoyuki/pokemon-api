8bd5666ea786b0d5f91767802904398592b1fc2e
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-26 23:10:41 +0900

    aysnc await実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {getAllPokemon} from "./utils/pokemon.js";
 
 function App() {
-  const initialURL = "https://pokeapi.co/api/v2/pokemon/";
+    const initialURL = "https://pokeapi.co/api/v2/pokemon/";
+    
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const fetchPokemonData = async () => {
             //すべてのポケモンデータの取得
             let res = await getAllPokemon(initialURL);
             console.log(res);
+            setLoading(false);
         }
         fetchPokemonData();
     }, []);
@@ -17,7 +20,9 @@ function App() {
 
   return (
     <>
-      <p>テスト</p>
+      <div className="App">
+          {loading ? (<h1>ロード中.........</h1>) : (<h1>ポケモンデータを取得しました</h1>)}
+      </div>
     </>
   )
 }
