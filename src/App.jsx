8c7b0b242bfdc907bfc056c7f92f1d0996514a1c
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-27 14:26:17 +0900

    Promise, async awaitで情報を持ってくる

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,42 @@
 import './App.css'
 import {useEffect, useState} from "react";
-import {getAllPokemon} from "./utils/pokemon.js";
+import {getAllPokemon, getPokemon} from "./utils/pokemon.js";
 
 function App() {
     const initialURL = "https://pokeapi.co/api/v2/pokemon/";
-    
+
     const [loading, setLoading] = useState(false);
+
+    const [pokemonData, setPokemonData] = useState([]);
 
     useEffect(() => {
         const fetchPokemonData = async () => {
             //すべてのポケモンデータの取得
             let res = await getAllPokemon(initialURL);
-            console.log(res);
+            //各ポケモンの詳細なデータを取得
+            // console.log("res")
+            // console.log(res);
+            loadPokemon(res.results);
+            // console.log(res.results);
+            // console.log(res.results);
             setLoading(false);
         }
         fetchPokemonData();
     }, []);
+
+    const  loadPokemon = async (data) => {
+        let _pokemonData = await Promise.all(
+            data.map((pokemon) => {
+                // console.log(pokemon);
+                let pokemonRecord = getPokemon(pokemon.url);
+                return pokemonRecord;
+            })
+        )
+        setPokemonData(_pokemonData);
+    };
+
+    console.log(pokemonData);
+
 
 
   return (
